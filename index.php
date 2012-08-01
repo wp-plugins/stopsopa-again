fd0960f5fc22c486f356b805ff3f2a6d0fae1dfc
scheeeli <scheeeli@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-01 18:22:13 +0000

    * Changed Widget from Stop SOPA to Defend Privacy with a whole new form.
    git-svn-id: https://plugins.svn.wordpress.org/stopsopa-again/tags/1.2.07.31@580362 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 /*
-Plugin Name: Stop SOPA Again! Sign the Petition Widget
+Plugin Name: Defend Privacy Widget! Call your Senators Now
 Plugin URI: http://wordpress.ieonly.com/category/my-plugins/stop-sopa-again/
 Author: Eli Scheetz
 Author URI: http://wordpress.ieonly.com/category/activism/
 Contributors: scheeeli
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8VWNB5QEJ55TJ
-Description: This Widget puts a petition form on your sidebar so that visitors can help fightforthefuture.org stop internet censorship legislation.
-Version: 1.2.04.06
+Description: Help spread the word and stop CISPA, the internet government surveillance system, before it becomes US law by added this Widget on your sidebar.
+Version: 1.2.07.31
 */
-$stopSOPA_Version='1.2.04.06';
+$stopSOPA_Version='1.2.07.31';
 $_SESSION['eli_debug_microtime']['include(stopSOPA)'] = microtime(true);
 /**
  * stopSOPA Main Plugin File
@@ -42,7 +42,7 @@ $_SESSION['eli_debug_microtime']['stopSOPA_install_end'] = microtime(true);
 class stopSOPA_Widget_Class extends WP_Widget {
 	function stopSOPA_Widget_Class() {
 $_SESSION['eli_debug_microtime']['stopSOPA_Widget_Class_Widget_Class_start'] = microtime(true);
-		$this->WP_Widget('stopSOPA-Widget', __('Stop SOPA Again!'), array('classname' => 'widget_stopSOPA', 'description' => __('Put this petition form on your sidebar to help get signatures against SOPA.')));
+		$this->WP_Widget('stopSOPA-Widget', __('Defend Privacy Widget!'), array('classname' => 'widget_stopSOPA', 'description' => __('Put this Widget on your sidebar help to stop CISPA, the internet government surveillance system, before it becomes US law.')));
 		$this->alt_option_name = 'widget_stopSOPA';
 $_SESSION['eli_debug_microtime']['stopSOPA_Widget_Class_Widget_Class_end'] = microtime(true);
 	}
@@ -51,94 +51,115 @@ $_SESSION['eli_debug_microtime']['_Widget_Class_widget_start'] = microtime(true)
 		$LIs = '';
 		extract($args);
 		if (!$instance['title'])
-			$instance['title'] = "Obama: Don't Help SOPA!";
+			$instance['title'] = "";
 		if (!$instance['number'])
 			$instance['number'] = 0;
 		if (!$instance['excerpt'])
-			$instance['excerpt'] = "Sign to make sure SOPA 2.0 never gets written.";
-		echo $before_widget.$before_title.$instance["title"].$after_title.'<div class="side">
-	<p>'.$instance['excerpt'].'</p>
-	<form class="ak-form" name="act" method="POST" target="_blank" action="http://a.fightforthefuture.org/act/" accept-charset="utf-8">
-	<input name="utf8" value="✔" type="hidden">
-	<style>
-	.gridded span input { width: 100%; height: 30px; margin: 2px; }
-	.gridded input[type="text" { height: 20px; }
-	</style>
-	    <div class="gridded">
-		    <span><input id="id_name" name="name" placeholder="Your Name" onfocus="if(this.value==\'Your Name\')this.value=\'\'" onblur="if(this.value==\'\')this.value=\'Your Name\'" value="Your Name" type="text"></span>
-			<span><input id="id_email" name="email" placeholder="Your Email" onfocus="if(this.value==\'Your Email\')this.value=\'\'" onblur="if(this.value==\'\')this.value=\'Your Email\'" value="Your Email" type="email"></span>
-			<span><input id="id_address1" name="address1" onfocus="if(this.value==\'Your Address\')this.value=\'\'" onblur="if(this.value==\'\')this.value=\'Your Address\'" value="Your Address" placeholder="Your Address" type="text"></span>
-<span><input id="id_city" name="city" onfocus="if(this.value==\'Your City\')this.value=\'\'" onblur="if(this.value==\'\')this.value=\'Your City\'" value="Your City" placeholder="Your City" type="text"></span>
-		    <div id="unknown_state"><span><select name="state" id="state">
-<option value="">State</option>
-<option value="AL">Alabama</option>
-<option value="AK">Alaska</option>
-<option value="AZ">Arizona</option>
-<option value="AR">Arkansas</option>
-<option value="CA">California</option>
-<option value="CO">Colorado</option>
-<option value="CT">Connecticut</option>
-<option value="DE">Delaware</option>
-<option value="FL">Florida</option>
-<option value="GA">Georgia</option>
-<option value="HI">Hawaii</option>
-<option value="ID">Idaho</option>
-<option value="IL">Illinois</option>
-<option value="IN">Indiana</option>
-<option value="IA">Iowa</option>
-<option value="KS">Kansas</option>
-<option value="KY">Kentucky</option>
-<option value="LA">Louisiana</option>
-<option value="ME">Maine</option>
-<option value="MD">Maryland</option>
-<option value="MA">Massachusetts</option>
-<option value="MI">Michigan</option>
-<option value="MN">Minnesota</option>
-<option value="MS">Mississippi</option>
-<option value="MO">Missouri</option>
-<option value="MT">Montana</option>
-<option value="NE">Nebraska</option>
-<option value="NV">Nevada</option>
-<option value="NH">New Hampshire</option>
-<option value="NJ">New Jersey</option>
-<option value="NM">New Mexico</option>
-<option value="NY">New York</option>
-<option value="NC">North Carolina</option>
-<option value="ND">North Dakota</option>
-<option value="OH">Ohio</option>
-<option value="OK">Oklahoma</option>
-<option value="OR">Oregon</option>
-<option value="PA">Pennsylvania</option>
-<option value="RI">Rhode Island</option>
-<option value="SC">South Carolina</option>
-<option value="SD">South Dakota</option>
-<option value="TN">Tennessee</option>
-<option value="TX">Texas</option>
-<option value="UT">Utah</option>
-<option value="VT">Vermont</option>
-<option value="VA">Virginia</option>
-<option value="WA">Washington</option>
-<option value="DC">Washington, D.C.</option>
-<option value="WV">West Virginia</option>
-<option value="WI">Wisconsin</option>
-<option value="WY">Wyoming</option>
-</select>
-</span></div>
-			<input id="id_zip" name="zip" size="5" onfocus="if(this.value==\'Your Zip\')this.value=\'\'" onblur="if(this.value==\'\')this.value=\'Your Zip\'" value="Your Zip" placeholder="Your Zip" type="text">
-
-		    <input name="country" value="United States" type="hidden">
-<br />More info at <a target="_blank" href="http://a.fightforthefuture.org/sign/obama-sopa/">fightforthefuture.org</a><br /><br />
-		<small>Fight for the Future may contact you about related campaigns. <a href="http://fightforthefuture.org/privacy">Privacy Policy</a>.</small>
-		
-		<input name="page" value="obama-sopa" type="hidden">
-		<span><input type="submit" value="Sign the Petition"></span>
-	</div>
-	<input name="lists" value="1" type="hidden">
-<input name="want_progress" value="1" type="hidden">
-
-<input name="want_progress" value="1" type="hidden">
-
-	<input value="act" name="form_name" type="hidden"><input value="http://a.fightforthefuture.org/sign/obama-sopa/" name="url" type="hidden"><input value="1" name="js" type="hidden"></form>
+			$instance['excerpt'] = "I don't want to give government agencies access to my emails. Vote no on s.2105 The Cybersecurity Act of 2012, and vote against any anti-privacy amendments to it. Also, vote for the pro-privacy Franken-Paul amendment.";
+		echo $before_widget.($instance["title"]?$before_title.$instance["title"].$after_title:'').'<style>.meter_box {
+margin-top: 15px;
+padding: 10px;
+-moz-border-radius: 5px;
+-webkit-border-radius: 5px;
+border-radius: 5px;
+-moz-background-clip: padding;
+-webkit-background-clip: padding-box;
+background-clip: padding-box;
+overflow: auto;
+}
+.sign {
+background-color: #065DA9;
+position: relative;
+top: 13px;
+ont-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+font-size: 16pt;
+text-decoration: none;
+font-weight: 700;
+padding: 5px;
+color: white;
+text-shadow: 0 1px 1px rgba(0, 0, 0, .5);
+-moz-border-radius: 5px;
+-webkit-border-radius: 5px;
+border-radius: 5px;
+-moz-background-clip: padding;
+-webkit-background-clip: padding-box;
+background-clip: padding-box;
+-moz-box-shadow: 0 3px 6px #3b2d06 /* drop shadow */, inset 0 1px 0 #7dbbf3 /* inner shadow */, 0 0 0 1px #0f71ca /* outer stroke */;
+-webkit-box-shadow: 0 3px 6px #3b2d06 /* drop shadow */, inset 0 1px 0 #7dbbf3 /* inner shadow */, 0 0 0 1px #0f71ca /* outer stroke */;
+box-shadow: 0 3px 6px #3b2d06 /* drop shadow */, inset 0 1px 0 #7dbbf3 /* inner shadow */, 0 0 0 1px #0f71ca /* outer stroke */;
+background-image: linear-gradient(bottom, #065DA9 27%, #5E9ED5 100%);
+background-image: -o-linear-gradient(bottom, #065DA9 27%, #5E9ED5 100%);
+background-image: -moz-linear-gradient(bottom, #065DA9 27%, #5E9ED5 100%);
+background-image: -webkit-linear-gradient(bottom, #065DA9 27%, #5E9ED5 100%);
+background-image: -ms-linear-gradient(bottom, #065DA9 27%, #5E9ED5 100%);
+background-image: -webkit-gradient( linear, left bottom, left top, color-stop(0.27, #065DA9), color-stop(1, #5E9ED5) );
+}
+</style>
+<div class="yellow"><h3>Call now to <strong>Defend Privacy</strong></h3>
+   <label for="state">State: </label>
+    <select id="state" name="state">
+        <option selected="selected">Select your state</option>
+    <option value="AK">Select your state: AK</option><option value="AL">Select your state: AL</option><option value="AR">Select your state: AR</option><option value="AZ">Select your state: AZ</option><option value="CA">Select your state: CA</option><option value="CO">Select your state: CO</option><option value="CT">Select your state: CT</option><option value="DE">Select your state: DE</option><option value="FL">Select your state: FL</option><option value="GA">Select your state: GA</option><option value="HI">Select your state: HI</option><option value="IA">Select your state: IA</option><option value="ID">Select your state: ID</option><option value="IL">Select your state: IL</option><option value="IN">Select your state: IN</option><option value="KS">Select your state: KS</option><option value="KY">Select your state: KY</option><option value="LA">Select your state: LA</option><option value="MA">Select your state: MA</option><option value="MD">Select your state: MD</option><option value="ME">Select your state: ME</option><option value="MI">Select your state: MI</option><option value="MN">Select your state: MN</option><option value="MO">Select your state: MO</option><option value="MS">Select your state: MS</option><option value="MT">Select your state: MT</option><option value="NC">Select your state: NC</option><option value="ND">Select your state: ND</option><option value="NE">Select your state: NE</option><option value="NH">Select your state: NH</option><option value="NJ">Select your state: NJ</option><option value="NM">Select your state: NM</option><option value="NV">Select your state: NV</option><option value="NY">Select your state: NY</option><option value="OH">Select your state: OH</option><option value="OK">Select your state: OK</option><option value="OR">Select your state: OR</option><option value="PA">Select your state: PA</option><option value="RI">Select your state: RI</option><option value="SC">Select your state: SC</option><option value="SD">Select your state: SD</option><option value="TN">Select your state: TN</option><option value="TX">Select your state: TX</option><option value="UT">Select your state: UT</option><option value="VA">Select your state: VA</option><option value="VT">Select your state: VT</option><option value="WA">Select your state: WA</option><option value="WI">Select your state: WI</option><option value="WV">Select your state: WV</option><option value="WY">Select your state: WY</option></select>
+    
+    <div id="senators"><div class="senator">
+            <h2><a href="http://www.akaka.senate.gov">Daniel Akaka (D)</a></h2>
+            <ul>
+                
+                    <li class="phone">Phone: <span id="gc-number-4" class="gc-cs-link" title="Call with Google Voice">202-224-6361</span></li>
+                
+                
+                
+                </ul>
+        </div><div class="senator">
+            <h2><a href="http://www.inouye.senate.gov/">Daniel Inouye (D)</a></h2>
+            <ul>
+                
+                    <li class="phone">Phone: <span id="gc-number-5" class="gc-cs-link" title="Call with Google Voice">202-224-3934</span></li>
+                
+                
+                
+                </ul>
+        </div></div>
+    
+    <script src="http://fightforthefuture.org/defendprivacy/js/jquery.js"></script>
+    <script src="http://fightforthefuture.org/defendprivacy/js/handlebars.js"></script>
+    <script src="http://fightforthefuture.org/defendprivacy/js/script.js"></script>
+    <script id="senator-template" type="text/x-handlebars-template">
+        <div class="senator">
+            <h2>{{#if website}}<a href="{{ website }}">{{/if}}{{ fullName }} ({{ party }}){{#if website}}</a>{{/if}}</h2>
+            <ul>
+                {{#if phone}}
+                    <li class="phone">Phone: {{ phone }}</li>
+                {{/if}}
+                {{#if email}}
+                    <li>Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
+                {{/if}}
+                
+                </ul>
+        </div>
+    </script>
+<h2>What to say:</h2>
+    <div class="meter_box">
+        <p>“'.$instance['excerpt'].'”</p>
+    </div>
+    <h2>Send an email too:</h2>
+    <form method="POST" action="http://nt.salsalabs.com/save">
+        <fieldset>
+      <input type="text" name="First_Name" placeholder="Your Name"><br>
+      <input type="text" name="Email" placeholder="Your email"><br>
+      <input type="text" name="Street" placeholder="Your address"><br>
+      <input type="text" name="Zip" placeholder="Your zip"><br>
+    <input type="hidden" name="organization_KEY" value="501">
+    <input type="hidden" name="redirect" value="http://fightforthefuture.org/defendprivacy/thanks"> 
+    <input type="hidden" name="object" value="supporter">
+    <input type="hidden" name="ip" id="signup_ip" value="72.235.77.94">
+    <input type="hidden" name="user_agent" id="user_agent" value="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.19 (KHTML, like Gecko) Ubuntu/11.04 Chromium/18.0.1025.151 Chrome/18.0.1025.151 Safari/535.19">
+    <input type="hidden" name="Receive_Email" value="3">	
+	<textarea>“'.$instance['excerpt'].'”</textarea>
+    <p class="caption">Fight for the Future may contact you about related campaigns. <a class="caption" href="http://fightforthefuture.org/privacy/">Privacy Policy.</a></p>
+</fieldset>
+        <button class="sign" type="submit">Send Email Now</button>
+</form>
 </div>'.$after_widget;
 $_SESSION['eli_debug_microtime']['stopSOPA_Widget_Class_widget_end'] = microtime(true);
 	}
@@ -149,7 +170,7 @@ $_SESSION['eli_debug_microtime']['stopSOPA_Widget_Class_widget_end'] = microtime
 		$instance = $old;
 		$instance['title'] = strip_tags($new['title']);
 		$instance['number'] = (int) $new['number'];
-		$instance['excerpt'] = strip_tags($new['excerpt']);
+		$instance['excerpt'] = ($new['excerpt']);
 		return $instance;
 	}
 	function form( $instance ) {
@@ -157,12 +178,10 @@ $_SESSION['eli_debug_microtime']['stopSOPA_Widget_Class_form_start'] = microtime
 		$title = isset($instance['title']) ? esc_attr($instance['title']) : '';
 		$number = isset($instance['number']) ? absint($instance['number']) : 0;
 		$excerpt = isset($instance['excerpt']) ? esc_attr($instance['excerpt']) : '';
-		echo '<p><label for="'.$this->get_field_id('title').'">'.__('Alternate Widget Title').':</label><br />
+		echo '<p><label for="'.$this->get_field_id('title').'">'.__('Widget Title (Optional)').':</label><br />
 		<input type="text" name="'.$this->get_field_name('title').'" id="'.$this->get_field_id('title').'" value="'.$title.'" /></p>
-		<p><label for="'.$this->get_field_id('excerpt').'">Your own introduction:</label><br />
+		<p><label for="'.$this->get_field_id('excerpt').'">Alternate "What to say":</label><br />
 		<textarea name="'.$this->get_field_name('excerpt').'" id="'.$this->get_field_id('excerpt').'" rows="5">'.$excerpt.'</textarea></p>';
-//		<p><label for="'.$this->get_field_id('excerpt').'">'.__('Show Excerpts').':</label>
-//		<input type="checkbox" name="'.$this->get_field_name('excerpt').'" id="'.$this->get_field_id('excerpt').'" value="yes"'.($excerpt=="yes"?" checked":"").' />yes</p>
 $_SESSION['eli_debug_microtime']['stopSOPA_Widget_Class_form_end'] = microtime(true);
 	}
 }
--- readme.txt
+++ readme.txt
@@ -1,23 +1,23 @@
-=== This Time Last Year Widget ===
-Plugin Name: Stop SOPA Again! Sign the Petition Widget
+=== Defend Privacy Widget! Call your Senators Now ===
+Plugin Name: Defend Privacy Widget! Call your Senators Now
 Plugin URI: http://wordpress.ieonly.com/category/my-plugins/stop-sopa-again/
 Author: Eli Scheetz
 Author URI: http://wordpress.ieonly.com/category/activism/
 Contributors: scheeeli
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8VWNB5QEJ55TJ
-Tags: widget, plugin, sidebar, form, petition, fightforthefuture, censorship, legislation, SOPA
-Stable tag: 1.2.04.06
-Version: 1.2.04.06
+Tags: widget, plugin, sidebar, form, petition, fightforthefuture, censorship, legislation, SOPA, CISPA, Privacy, Senator
+Stable tag: 1.2.07.31
+Version: 1.2.07.31
 Requires at least: 2.6
-Tested up to: 3.3.1
+Tested up to: 3.4.1
 
-This Widget puts a petition form on your sidebar so that visitors can help fightforthefuture.org stop internet censorship legislation.
+Help spread the word and stop CISPA, the internet government surveillance system, before it becomes US law by added this Widget on your sidebar.
 
 == Description ==
 
-Just place the Widget on your sidebar to display the petition form. Results are collected by fightforthefuture.org
+Just place the Widget on your sidebar to display the contact info for your Senator and help spread the word to stop CISPA, the internet government surveillance system, before it becomes US law.
 
-Plugin Updated Apr-6th
+Plugin Updated July-31st
 
 == Installation ==
 
@@ -29,19 +29,31 @@ Plugin Updated Apr-6th
 
 = What do I do after I activate the Plugin? =
 
-Go to the Widgets menu in the WordPress Admin and add the "This Time Last Year" Widget to your Sidebar or Footer Area.
+Go to the Widgets menu in the WordPress Admin and add "Defend Privacy Widget" to your Sidebar or Footer Area.
 
 = Where can I get more information about this? =
 
-start here http://a.fightforthefuture.org/sign/obama-sopa/
+start here http://fightforthefuture.org/defendprivacy/
 
 == Changelog ==
 
-= 1.2.04.06 =
+= 1.2.07.31 =
+* Changed "Stop SOPA Widget" to "Defend Privacy Widget" with a whole new form.
+
+= 1.2.04.08 =
+* Fixed "Your own introduction" field to not filter tags.
+
+= 1.2.04.07 =
 * First versions uploaded to WordPress.
 
 == Upgrade Notice ==
 
-= 1.2.04.06 =
+= 1.2.07.31 =
+Changed Widget from Stop SOPA to Defend Privacy with a whole new form.
+
+= 1.2.04.08 =
+Fixed "Your own introduction" field to not filter tags.
+
+= 1.2.04.07 =
 First versions available through WordPress.
 
